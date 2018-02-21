351cedde8a689aba4098c63982697fa039a5c9af
knite51 <knite51@gmail.com>
2018-02-21 15:55:53 +0100

    Test

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -10,9 +10,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    window.open('abksales://atbtechsoft.com', '_system', 'location=no');
+    // window.open('abksales://atbtechsoft.com', '_system', 'location=no');
     // window.open('abksales://', '_system', 'location=no');
-    // window.open('atbtechsoft.com/abksales',, '_system', 'location=no');
+    window.open('https://atbtechsoft.com/abksales',, '_system', 'location=no');
   }
 
 }
